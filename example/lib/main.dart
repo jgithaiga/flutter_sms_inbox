e2f57ceae440afcfd69853b981243e4a886a2f35
jgithaiga <juliusgthg@gmail.com>
2020-06-18 16:09:21 +0300

    Update example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
 
 void main() => runApp(MyApp());
@@ -12,32 +9,26 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  final SmsQuery _query = SmsQuery();
+  List<SmsMessage> _messages = List<SmsMessage>();
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterSmsInbox.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+  Widget _showMessagesList() {
+    return ListView.builder(
+      itemCount: _messages?.length,
+      itemBuilder: (BuildContext context, int i) {
+        var message = _messages[i];
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
+        return ListTile(
+          title: Text('#${message.id} from ${message.address}'),
+          subtitle: Text(message.body),
+        );
+      },
+    );
   }
 
   @override
@@ -45,10 +36,25 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Example App'),
+          actions: <Widget>[
+            OutlineButton(
+              child: Text('READ 10 MESSAGES'),
+              color: Theme.of(context).primaryColor,
+              textTheme: ButtonTextTheme.primary,
+              onPressed: () async {
+                var messages = await _query.querySms(
+                  kinds: [SmsQueryKind.Inbox],
+                  count: 10,
+                );
+                setState(() => _messages = messages);
+              },
+            ),
+          ],
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Container(
+          padding: EdgeInsets.all(10.0),
+          child: _showMessagesList(),
         ),
       ),
     );
