2bbcbe93c7b0a12cdb880ff0c57e153953a177db
jgithaiga <juliusgthg@gmail.com>
2021-10-27 02:34:40 +0300

    Update the sample app to show a list of messages

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:flutter_sms_inbox/flutter_sms_inbox.dart';
 
 void main() {
@@ -16,34 +13,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  final SmsQuery _query = SmsQuery();
+  List<SmsMessage> _messages = [];
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await FlutterSmsInbox.platformVersion ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
@@ -53,8 +28,32 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+        body: Container(
+          padding: const EdgeInsets.all(10.0),
+          child: ListView.builder(
+            shrinkWrap: true,
+            itemCount: _messages.length,
+            itemBuilder: (BuildContext context, int i) {
+              var message = _messages[i];
+
+              return ListTile(
+                title: Text('${message.sender}'),
+                subtitle: Text('${message.body}'),
+              );
+            },
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final messages = await _query.querySms(
+              kinds: [SmsQueryKind.Inbox, SmsQueryKind.Sent],
+              count: 10,
+            );
+            print('sms inbox messages: $messages');
+
+            setState(() => _messages = messages);
+          },
+          child: const Icon(Icons.refresh),
         ),
       ),
     );
