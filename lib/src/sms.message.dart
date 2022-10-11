756ad7ded9f789cb8d7f55af03761f51611faa19
jgithaiga <juliusgthg@gmail.com>
2022-10-12 01:25:21 +0300

    remove unnecessary getters and setters

--- lib/src/sms.message.dart
+++ lib/src/sms.message.dart
@@ -2,102 +2,75 @@ part of flutter_sms_inbox;
 
 /// Message
 class SmsMessage implements Comparable<SmsMessage> {
-  int? _id;
-  int? _threadId;
-  String? _address;
-  String? _body;
-  bool? _read;
-  DateTime? _date;
-  DateTime? _dateSent;
-  SmsMessageKind? _kind;
+  int? id;
+  int? threadId;
+  String? address;
+  String? body;
+  bool? read;
+  DateTime? date;
+  DateTime? dateSent;
+  SmsMessageKind? kind;
   SmsMessageState _state = SmsMessageState.none;
   final StreamController<SmsMessageState> _stateStreamController =
       StreamController<SmsMessageState>();
 
-  SmsMessage(
-    this._address,
-    this._body, {
-    int? id,
-    int? threadId,
-    bool? read,
-    DateTime? date,
-    DateTime? dateSent,
-    SmsMessageKind? kind,
-  }) {
-    _id = id;
-    _threadId = threadId;
-    _read = read;
-    _date = date;
-    _dateSent = dateSent;
-    _kind = kind;
-  }
-
   SmsMessage.fromJson(Map data) {
-    _address = data["address"];
-    _body = data["body"];
+    address = data["address"];
+    body = data["body"];
     if (data.containsKey("_id")) {
-      _id = data["_id"];
+      id = data["_id"];
     }
     if (data.containsKey("thread_id")) {
-      _threadId = data["thread_id"];
+      threadId = data["thread_id"];
     }
     if (data.containsKey("read")) {
-      _read = (data["read"].toInt() == 1);
+      read = (data["read"].toInt() == 1);
     }
     if (data.containsKey("kind")) {
-      _kind = data["kind"];
+      kind = data["kind"];
     }
     if (data.containsKey("date")) {
-      _date = DateTime.fromMillisecondsSinceEpoch(data["date"]);
+      date = DateTime.fromMillisecondsSinceEpoch(data["date"]);
     }
     if (data.containsKey("date_sent")) {
-      _dateSent = DateTime.fromMillisecondsSinceEpoch(data["date_sent"]);
+      dateSent = DateTime.fromMillisecondsSinceEpoch(data["date_sent"]);
     }
   }
 
   /// Convert SMS to map
   Map get toMap {
     Map data = {};
-    if (_address != null) {
-      data["address"] = _address;
+    if (address != null) {
+      data["address"] = address;
     }
-    if (_body != null) {
-      data["body"] = _body;
+    if (body != null) {
+      data["body"] = body;
     }
-    if (_id != null) {
-      data["_id"] = _id;
+    if (id != null) {
+      data["_id"] = id;
     }
-    if (_threadId != null) {
-      data["thread_id"] = _threadId;
+    if (threadId != null) {
+      data["thread_id"] = threadId;
     }
-    if (_read != null) {
-      data["read"] = _read;
+    if (read != null) {
+      data["read"] = read;
     }
-    if (_date != null) {
-      data["date"] = _date!.millisecondsSinceEpoch;
+    if (date != null) {
+      data["date"] = date!.millisecondsSinceEpoch;
     }
-    if (_dateSent != null) {
-      data["dateSent"] = _dateSent!.millisecondsSinceEpoch;
+    if (dateSent != null) {
+      data["dateSent"] = dateSent!.millisecondsSinceEpoch;
     }
     return data;
   }
 
   /// Getters
-  int? get id => _id;
-  int? get threadId => _threadId;
-  String? get sender => _address;
-  String? get address => _address;
-  String? get body => _body;
-  bool? get isRead => _read;
-  DateTime? get date => _date;
-  DateTime? get dateSent => _dateSent;
-  SmsMessageKind? get kind => _kind;
+  String? get sender => address;
+  bool? get isRead => read;
   SmsMessageState get state => _state;
   Stream<SmsMessageState> get onStateChanged => _stateStreamController.stream;
 
   /// Setters
-  set kind(SmsMessageKind? kind) => _kind = kind;
-  set date(DateTime? date) => _date = date;
   set state(SmsMessageState state) {
     if (_state != state) {
       _state = state;
@@ -107,6 +80,6 @@ class SmsMessage implements Comparable<SmsMessage> {
 
   @override
   int compareTo(SmsMessage other) {
-    return other._id! - _id!;
+    return other.id! - id!;
   }
 }
